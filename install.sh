5b5fa21fb88b5780c4aeccf0937826eafe8b8e01
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-09-20 19:34:23 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ echo "removing previous install if it exists"
 
 cd $HOME
 
-sudo rm -rf $HOME/homebrew/plugins/SDH-PauseGames
+sudo rm -rf "$HOME/homebrew/plugins/Pause Games"
 
 echo "installing SDH-PauseGames plugin for TDP control"
 # download + install simple decky tdp
-curl -L $(curl -s https://api.github.com/repos/aarron-lee/SDH-PauseGames/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/SDH-PauseGames.zip
-sudo 7z x ./SDH-PauseGames.zip  -o$HOME/homebrew/plugins
+curl -L $(curl -s https://api.github.com/repos/aarron-lee/SDH-PauseGames/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/PauseGames.zip
+sudo 7z x ./PauseGames.zip  -o$HOME/homebrew/plugins
 
 # install complete, remove build dir
 rm  $HOME/SDH-PauseGames.zip
